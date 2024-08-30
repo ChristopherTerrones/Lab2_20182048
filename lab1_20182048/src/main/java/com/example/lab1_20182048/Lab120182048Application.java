57b879cff519568a70b1af8dbcba3a0cb4e3aa35
ChristopherTerrones <a20182048@pucp.edu.pe>
2024-08-29 19:23:51 -0500

    Commit inicial

---
+++ lab1_20182048/src/main/java/com/example/lab1_20182048/Lab120182048Application.java
@@ -0,0 +1,13 @@
+package com.example.lab1_20182048;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab120182048Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab120182048Application.class, args);
+    }
+
+}
---
+++ lab1_20182048/src/test/java/com/example/lab1_20182048/Lab120182048ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab1_20182048;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab120182048ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
